03542be011f363f77bbd88bf3c70f6f56e7cd098
yolo <2389565044@qq.com>
2026-01-10 14:09:47 +0800

    测试Sting

---
+++ untitled/src/js.java
@@ -0,0 +1,11 @@
+public class js {
+    public static void main(String[] args) {
+        int s = 12;
+        change(s);
+        System.out.print(s);
+    }
+    public static void change(int s){
+        s=123;
+
+    }
+}
